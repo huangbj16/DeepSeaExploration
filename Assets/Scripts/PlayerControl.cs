5aa1b1d7952a2530672a8a8d4afb28e76e57897e
Bingjian Huang <huangbj16@gmail.com>
2023-12-26 11:30:25 -0500

    reconfigure controller input

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public AudioSource collisionAudioEffect;
+    public GameObject mainCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,8 @@ public class PlayerControl : MonoBehaviour
         float verticalForce = Input.GetAxis("Vertical");
         float horizontalForce = Input.GetAxis("Horizontal");
         float throttleForce = Input.GetAxis("RightTrigger");
+        float lookUp = Input.GetAxis("LookUp");
+        float lookRight = Input.GetAxis("LookRight");
         bool isUnderControl = false;
         Debug.Log("vertical = " + verticalForce + ", horizontal = "+ horizontalForce + ", RT = " + throttleForce);
         if (Mathf.Abs(verticalForce) >= 1e-6)
@@ -35,7 +38,20 @@ public class PlayerControl : MonoBehaviour
             isUnderControl = true;
             this.transform.position += this.transform.forward * throttleForce * Time.deltaTime;
         }
-        
+        Debug.Log("lookup = " + lookUp + ", lookright = " + lookRight);
+        if (Mathf.Abs(lookUp) >= 1e-6 || Mathf.Abs(lookRight) >= 1e-6)
+        {
+            isUnderControl = true;
+            Vector3 rotationVector = new Vector3(0, 45, 0) * lookRight * Time.deltaTime + new Vector3(45, 0, 0) * lookUp * Time.deltaTime;
+            mainCamera.transform.Rotate(rotationVector);
+            mainCamera.transform.localRotation = Quaternion.Euler(mainCamera.transform.localRotation.eulerAngles[0], mainCamera.transform.localRotation.eulerAngles[1], 0);
+            //mainCamera.transform.localRotation = Quaternion.Euler(rotationVector);
+        }
+        else
+        {
+            mainCamera.transform.localRotation = Quaternion.identity;
+        }
+
         this.transform.rotation = Quaternion.Euler(0, this.transform.rotation.eulerAngles[1], 0);
         if (isUnderControl)//override the rigidbody collision effect if under control
         {
